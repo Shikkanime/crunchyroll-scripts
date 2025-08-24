117614c65c04a217111a12a3eeb2a2d2603c1c3b
bytedream <me@bytedream.dev>
2025-08-25 01:02:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# crunchyroll-scripts
+A collection of handy bash scripts to simplify some interactions with Crunchyroll.
+
+### → [apk-credentials-extract](./apk-credentials-extract.sh)
+Extracts client id & client secret from a Crunchyroll App APK. You can either dump an apk from your own device or use a website like [apkpure.com](https://apkpure.com/) or [aptoide.com](https://aptoide.com/) to download them.
+
+<sub>Inspired from [vitalygashkov/crextractor](https://github.com/vitalygashkov/crextractor), but more lightweight and without needing a whole JS runtime.</sub>
---
+++ apk-credentials-extract.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <APK FILE>"
+    exit 1
+fi
+
+# verify that the input file exists
+echo "[·] Verifying input"
+if [ ! -f "$1" ]; then
+    echo "File '$1' doesn't exist"
+    exit 1
+fi
+
+# verify that all necessary commands are present
+echo "[·] Verifying environment"
+set +e
+which jadx &> /dev/null
+set -e
+if [ $? -ne 0 ]; then
+    echo "Command 'jadx' not found. Verify that it's installed and accessible by this script"
+    exit 1
+fi
+
+# variables
+decompile_sources_dir="$(mktemp -d)"
+
+# decompile the apk into the
+echo "[·] Decompiling file (may print 'ERROR - finished with errors, count: XXX', this can safely be ignored)"
+set +e
+jadx \
+    --output-dir-src "$decompile_sources_dir" \
+    --no-res \
+    "$1"
+set -e
+
+# extract the secrets out of the decompiled sources
+if [ -f "$decompile_sources_dir/com/crunchyroll/api/util/Constants.java" ]; then
+    echo "[·] Extracting secrets"
+    client_id="$(cat "$decompile_sources_dir/com/crunchyroll/api/util/Constants.java" | grep -oP '(?<=\sPROD_CLIENT_ID\s=\s")\S+(?=";)')"
+    client_secret="$(cat "$decompile_sources_dir/com/crunchyroll/api/util/Constants.java" | grep -oP '(?<=\sPROD_CLIENT_SECRET\s=\s")\S+(?=";)')"
+else
+    echo "[·] Extracting secrets, this may take a while"
+    decompiled_file="$(find "$decompile_sources_dir" -type f -exec sh -c "cat '{}' | grep -q ' ConfigurationImpl.kt' && echo {}" \;)"
+
+    constants="$(cat "$decompiled_file" | grep -n -oP '(?<==\s")\S+(?=")')"
+    marker_index=$(echo "$constants" | grep -oP '\d+(?=:https://sso.crunchyroll.com)')
+
+    client_id="$(echo $constants | grep -oP "(?<=$(($marker_index + 2)):)\S+")"
+    client_secret="$(echo $constants | grep -oP "(?<=$(($marker_index + 3)):)\S+")"
+fi
+basic_auth_creds="$(echo "$client_id:$client_secret" | base64)"
+
+# cleanup decompiled sources
+echo "[·] Cleaning up"
+rm -r "$decompile_sources_dir"
+
+# print results
+echo "[·] Finished"
+echo "client id: $client_id"
+echo "client secret: $client_secret"
+echo "basic auth credentials: $basic_auth_creds"
